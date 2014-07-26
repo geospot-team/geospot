03a5a6c5144897b7d171e34a0ba1b2d7dd35cfeb
Vasily <vasilij.ershov@gmail.com>
2014-07-27 01:01:35 +0400

    merge twitter fixes to mongo_crawler

--- gathering/Twitter/tweets_crawler_mongo.py
+++ gathering/Twitter/tweets_crawler_mongo.py
@@ -1,14 +1,15 @@
+import json
 import time
 import sys
-import json
-from json import JSONEncoder
+import calendar
+from datetime import datetime
 
 import pymongo
 
+import simplejson
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-
 
 config = json.loads(open(sys.argv[1]).read())
 
@@ -17,16 +18,18 @@ CSECRET = config["twitter_auth"]["CSECRET"]
 ATOKEN = config["twitter_auth"]["ATOKEN"]
 ASECRET = config["twitter_auth"]["ASECRET"]
 
-to_save_main = ["created_at", "id", "text", "source", "geo", "retweet_count", "favorite_count"]
+to_save_main = ["id", "text", "retweet_count", "favorite_count"]
 to_save = {
-    "user": ["id", "followers_count", "friends_count", "listed_count", "created_at", "favourites_count"],
-    "place": ["id", "url", "bounding_box"],
-    "entities": ["hashtags"]
+    "user": ["id", "followers_count", "friends_count", "listed_count", "favourites_count"],
+    "place": ["id", "url", "bounding_box"]
 }
-to_save_entities = {
-    "urls": "expanded_url",
-    "user_mentions": "id",
-}
+
+sourse = ["Web", "iPhone", "iPad", "Android", "instagram", "foursquare"]
+
+
+def to_timestamp(date):
+    dt = datetime.strptime(date, "%a %b %d %H:%M:%S +0000 %Y")
+    return calendar.timegm(dt.timetuple())
 
 
 class Listener(StreamListener):
@@ -48,21 +51,43 @@ class Listener(StreamListener):
 
     def on_data(self, data):
         try:
-            content = json.loads(data)
-            content_dict = {}
+            content = simplejson.loads(data)
 
+            content_dict = {}
             for el in to_save_main:
                 content_dict[el] = content[el]
             for key in to_save:
                 content_dict[key] = {}
                 for el in to_save[key]:
                     content_dict[key][el] = content[key][el]
-            for key in to_save_entities:
-                entity = content["entities"][key]
-                ent_key = to_save_entities[key]
-                content_dict[key] = {}
-                if len(entity) != 0:
-                    content_dict[key][ent_key] = entity[0][ent_key]
+
+            tweet_sourse = content["source"]
+            for item in sourse:
+                if item in tweet_sourse:
+                    content_dict["source"] = item
+                    break
+
+            geo = content["geo"]
+            if geo != None:
+                content_dict["geo"] = geo["coordinates"]
+            else:
+                # 1st method
+                bbox = content["place"]["bounding_box"]["coordinates"][0]
+                content_dict["geo"] = [(bbox[0][0] + bbox[1][0] + bbox[2][0] + bbox[3][0]) / 4,
+                                       (bbox[0][1] + bbox[1][1] + bbox[2][1] + bbox[3][1]) / 4]
+                # 2nd method
+                # content_dict["geo"] = content["place"]["bounding_box"]["coordinates"][0]
+
+                # 3rd method
+                #content_dict["geo"] = content["place"]["bounding_box"]["coordinates"]
+
+            content_dict["created_at"] = to_timestamp(content["created_at"])
+            content_dict["user"]["created_at"] = to_timestamp(content["user"]["created_at"])
+            content_dict["hashtags"] = content["entities"]["hashtags"]
+
+            if content_dict["source"] in ["instagram", "foursquare"]:
+                content_dict["url"] = content["entities"]["urls"][0]["expanded_url"]
+
             content_dict["_id"] = content_dict["id"]
             del (content_dict["id"])
             self.batch.append(content_dict)
